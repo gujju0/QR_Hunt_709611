afa39332723b51932eac69ce67f0b084f3786cd7
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:34:20 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does the term PLC stand for?",
+    answer: "Jumbo",
+    options: [
+      "AV003-A",
+      "AV012-D",
+      "AV008-B",
+      "AV113-D"
+    ]
+  },
+  {
+    numb: 2,
+    question: "In which year was MIDI(Musical Instrument Digital Interface) introduced?",
+    answer: "Antivirus",
+    options: [
+      "AV005-B",
+      "AV102-C",
+      "AV012-B",
+      "AV109-D"
+    ]
+  },
+  {
+    numb: 3,
+    question: "A given signal's second harmonic is twice the given signal's __________ frequency...?",
+    answer: "Macro",
+    options: [
+      "AV007-B",
+      "AV001-C",
+      "AV114-C",
+      "AV106-A"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which motor is NOT suitable for use as a DC machine?",
+    answer: "Blockchain",
+    options: [
+      "AV104-D",
+      "AV006-D",
+      "AV102-D",
+      "AV112-C"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "NFT",
+    options: [
+      "AV002-B",
+      "AV111-C",
+      "AV004-D",
+      "AV001-C"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What frequency range is the High Frequency band?",
+    answer: "Big data",
+    options: [
+      "AV107-B",
+      "AV114-C",
+      "AV008-A",
+      "AV104-C"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "The most common format for a home video recorder is VHS. VHS stands for...?",
+    answer: "Python",
+    options: [
+      "AV112-A",
+      "AV005-C",
+      "AV010-C",
+      "AV104-A"
+    ]
+  },
+  
+];
